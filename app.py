dcfe8e39f50b89d440702c2507df8d7fdc2107e8
vregesifrn <161090055+vregesifrn@users.noreply.github.com>
2026-06-15 02:20:35 +0000

    Implementa exemplos do Quickstart do Flask

--- app.py
+++ app.py
@@ -1,31 +1,47 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, redirect, abort, session
 from markupsafe import escape
 
 app = Flask(__name__)
+
+
+app.secret_key = "chave-secreta"
+
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
+
 @app.route("/about")
 def about():
     return "Página sobre"
 
+
 @app.route("/user/<name>")
 def user(name):
-    return f"Olá {name}"
+    return f"Olá {escape(name)}"
+
+
 
 @app.route("/escape/<name>")
 def escape_name(name):
     return f"Olá {escape(name)}"
 
+
+
 @app.route("/static-test")
 def static_test():
     return url_for("static", filename="style.css")
 
+
+
 @app.route("/home")
 def home():
     return render_template("index.html", name="Vitória")
+
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -33,21 +49,41 @@ def login():
         return f"Usuário {request.form['username']}"
     return "Envie um POST"
 
-from flask import redirect, abort, url_for
+
 
 @app.route("/go")
 def go():
-    return redirect(url_for("hello"))
+    return redirect(url_for("hello_world"))
+
 
 @app.route("/erro")
 def erro():
     abort(404)
 
-from flask import session
 
-app.secret_key = "chave-secreta"
 
-@app.route("/set")
-def set_session():
-    session["user"] = "FlaskUser"
-    return "Sessão criada"
+@app.route("/session")
+def index():
+    if "username" in session:
+        return f"Logged in as {session['username']}"
+    return "Você não está logado"
+
+
+@app.route("/session/login", methods=["GET", "POST"])
+def session_login():
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        return redirect(url_for("index"))
+
+    return """
+        <form method="post">
+            <input name="username">
+            <button type="submit">Login</button>
+        </form>
+    """
+
+
+@app.route("/session/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("index"))
