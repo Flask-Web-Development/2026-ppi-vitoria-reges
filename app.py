489023530f875944f411a2a63d9f4eb52d405755
vregesifrn <161090055+vregesifrn@users.noreply.github.com>
2026-06-15 02:11:37 +0000

    Organiza imports e estrutura do Quickstart

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, request
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-    @app.route("/about")
+@app.route("/about")
 def about():
     return "Página sobre"
 
@@ -14,25 +15,17 @@ def about():
 def user(name):
     return f"Olá {name}"
 
-from markupsafe import escape
-
 @app.route("/escape/<name>")
 def escape_name(name):
     return f"Olá {escape(name)}"
-
-from flask import url_for
 
 @app.route("/static-test")
 def static_test():
     return url_for("static", filename="style.css")
 
-from flask import render_template
-
 @app.route("/home")
 def home():
     return render_template("index.html", name="Vitória")
-
-from flask import request
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
