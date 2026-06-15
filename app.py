7940a8dbe49954f2bf8f3c151beb5111d94fdde2
vregesifrn <161090055+vregesifrn@users.noreply.github.com>
2026-06-15 02:00:29 +0000

    Adiciona rotas básicas

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+    @app.route("/about")
+def about():
+    return "Página sobre"
+
+@app.route("/user/<name>")
+def user(name):
+    return f"Olá {name}"
